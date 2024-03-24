a03135fddd81aeeda4662d6be22812e9b5152ed0
Peter Dinis <peterdinis0@pobox.sk>
2024-03-24 08:53:40 +0100

    Delete post router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,9 +1,7 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { bookRouter } from "./routers/book";
 
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   book: bookRouter
 });
 
