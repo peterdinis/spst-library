ed756abe5e669fd527d8e83e59e6c2dabff54dfe
Peter Dinis <peterdinis0@pobox.sk>
2024-03-24 14:38:55 +0100

    feat: Adding new content WIP

---
+++ src/app/_components/authors/AllAuthors.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+const AllAuthors: FC = () => {
+    return (
+        <></>
+    )
+}
+
+export default AllAuthors;
--- src/app/_components/categories/AllCategories.tsx
+++ src/app/_components/categories/AllCategories.tsx
@@ -1,7 +1,12 @@
 import { FC } from 'react';
+import Header from '../shared/Header';
 
 const AllCategories: FC = () => {
-    return <>aaaaa</>;
+    return (
+        <>
+            <Header text='Všetky kategórie' />
+        </>
+    )
 };
 
 export default AllCategories;
