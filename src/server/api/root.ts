39814c3863643bc09a870863ff536177e0b19593
Peter Dinis <peterdinis0@pobox.sk>
2024-03-27 16:29:26 +0100

    infra: New routes

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,8 +1,14 @@
 import { createCallerFactory, createTRPCRouter } from '~/server/api/trpc';
 import { bookRouter } from './routers/book';
+import { categoryRouter } from './routers/category';
+import { publisherRouter } from './routers/publisher';
+import { authorRouter } from './routers/author';
 
 export const appRouter = createTRPCRouter({
     book: bookRouter,
+    category: categoryRouter,
+    publisher: publisherRouter,
+    author: authorRouter
 });
 
 export type AppRouter = typeof appRouter;
---
+++ src/server/api/routers/category.ts
@@ -0,0 +1,10 @@
+import { TRPCError } from '@trpc/server';
+import { createTRPCRouter, publicProcedure } from '../trpc';
+import { z } from 'zod';
+
+export const categoryRouter = createTRPCRouter({
+    fetchCategories: publicProcedure.query(async({ctx}) => {
+        const categories = await ctx.db.category.findMany();
+        return categories;
+    })
+})
