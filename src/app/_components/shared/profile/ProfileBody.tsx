f8c18843478b5c469fe8e046e9d743b46260740d
Peter Dinis <peterdinis0@pobox.sk>
2024-03-28 09:39:06 +0100

    feat: Prepare first markup components for profile

--- src/app/_components/shared/profile/ProfileBody.tsx
+++ src/app/_components/shared/profile/ProfileBody.tsx
@@ -1,10 +1,34 @@
-import { FC } from "react";
+import { User2 } from 'lucide-react';
+import { FC } from 'react';
 
-const ProfileBody: FC = () => {
-    return (
-        <></>
-    )
+interface IProfileBodyProps {
+    name: string;
+    lastName: string;
 }
 
+const ProfileBody: FC<IProfileBodyProps> = ({ name, lastName }) => {
+    return (
+        <div className='h-128 mx-2 w-full md:w-9/12'>
+            <div className='rounded-sm bg-white p-3 shadow-sm'>
+                <div className='flex items-center space-x-2 leading-8 text-gray-900'>
+                    <User2 className='h-8 w-8' />
+                    <span className='tracking-wide'>Základné informácie</span>
+                </div>
+                <div className='text-gray-700'>
+                    <div className="grid md:grid-cols-2 text-sm">
+                        <div className="grid grid-cols-2">
+                            <div className="px-4 py-2">Meno</div>
+                            <div className="px-4 py-2">{name}</div>
+                        </div>
+                        <div className="grid grid-cols-2">
+                            <div className="px-4 py-2">Priezvisko</div>
+                            <div className="px-4 py-2">{lastName}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default ProfileBody;
--- src/app/_components/student/ProfileWrapper.tsx
+++ src/app/_components/student/ProfileWrapper.tsx
@@ -1,7 +1,14 @@
 import { FC } from 'react';
+import ProfileHeader from '../shared/profile/ProfileHeader';
+import ProfileBody from '../shared/profile/ProfileBody';
 
 const ProfileWrapper: FC = () => {
-    return <>PROFILE</>;
+    return (
+        <>
+            <ProfileHeader email={"random@gmail.com"} />
+            <ProfileBody name="Random" lastName='MR' />
+        </>
+    )
 };
 
 export default ProfileWrapper;
