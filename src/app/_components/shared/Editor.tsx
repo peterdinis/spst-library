a4a253664736339f3108627bb61f69b7c8bb5f89
Peter Dinis <peterdinis0@pobox.sk>
2024-03-28 13:46:36 +0100

    FOO

--- src/app/_components/shared/Editor.tsx
+++ src/app/_components/shared/Editor.tsx
@@ -1,40 +1,10 @@
 'use client';
 
-import { FC, useState, useRef, useCallback, useMemo } from 'react';
+import { FC, useState, useMemo } from 'react';
 import QuillEditor from 'react-quill';
 
 const Editor: FC = () => {
     const [value, setValue] = useState<string>('Popis pre knihu');
-    const quill = useRef<any | null>(null);
-
-    const imageHandler = useCallback(() => {
-        const input = document.createElement('input');
-        input.setAttribute('type', 'file');
-        input.setAttribute('accept', 'image/*');
-        input.click();
-
-        input.onchange = () => {
-            const file = input.files![0] as any;
-            const reader = new FileReader();
-
-            reader.onload = () => {
-                const imageUrl = reader.result;
-                const quillEditor = quill.current?.getEditor();
-
-                if (quillEditor) {
-                    const range = quillEditor.getSelection(true);
-                    quillEditor.insertEmbed(
-                        range?.index || 0,
-                        'image',
-                        imageUrl,
-                        'user',
-                    );
-                }
-            };
-
-            reader.readAsDataURL(file);
-        };
-    }, [quill]);
 
     const modules = useMemo(
         () => ({
@@ -52,15 +22,12 @@ const Editor: FC = () => {
                     ['link', 'image'],
                     ['clean'],
                 ],
-                handlers: {
-                    image: imageHandler,
-                },
             },
             clipboard: {
                 matchVisual: true,
             },
         }),
-        [imageHandler],
+        [],
     );
 
     const formats = [
@@ -82,8 +49,6 @@ const Editor: FC = () => {
     return (
         <div className='mt-4'>
             <QuillEditor
-                // className='peer mt-4 block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-lg text-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600'
-                ref={(el: any) => (quill.current = el)}
                 theme='snow'
                 value={value}
                 formats={formats}
