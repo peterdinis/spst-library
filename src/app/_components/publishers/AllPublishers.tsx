f0286215fb95ec158a0fbfc39a1802be6095c12d
Peter Dinis <peterdinis0@pobox.sk>
2024-03-29 07:51:46 +0100

    FOO

--- src/app/_components/publishers/AllPublishers.tsx
+++ src/app/_components/publishers/AllPublishers.tsx
@@ -1,7 +1,33 @@
-import { FC } from 'react';
+"use client"
+
+import { FC, useState } from 'react';
 import Header from '../shared/Header';
+import { api } from '~/trpc/react';
+import { Loader2, Ghost } from 'lucide-react';
 
 const AllPublishers: FC = () => {
+    const {data, isLoading, isError} = api.publisher.fetchPublishers.useQuery();
+    const [searchTerm, setSearchTerm] = useState('');
+    
+    if (isLoading) {
+        return <Loader2 className='h-8 w-8 animate-spin' />;
+    }
+
+    if (isError) {
+        return (
+            <>
+                <Ghost className='h-8 w-8 animate-bounce' />{' '}
+                <span className='font-bold'>Vydavateľstvá neboli nájdené</span>
+            </>
+        );
+    }
+
+    const filteredData =
+        data &&
+        data.filter((item: any) =>
+            item.name.toLowerCase().includes(searchTerm.toLowerCase()),
+        );
+    
     return (
         <>
             <Header text='Všetky vydavateľstvá' />
--- src/server/api/routers/publisher.ts
+++ src/server/api/routers/publisher.ts
@@ -1,3 +1,8 @@
-import { createTRPCRouter } from '../trpc';
+import { createTRPCRouter, publicProcedure } from '../trpc';
 
-export const publisherRouter = createTRPCRouter({});
+export const publisherRouter = createTRPCRouter({
+    fetchPublishers: publicProcedure.query(async ({ ctx }) => {
+        const publishers = await ctx.db.publisher.findMany({});
+        return publishers;
+    }),
+});
