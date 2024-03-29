fbd743a0841cbfa1f7e542c509d217bd2ecfc1a6
Peter Dinis <peterdinis0@pobox.sk>
2024-03-29 14:34:31 +0100

    style: Update card

--- src/app/_components/shared/GlobalCard.tsx
+++ src/app/_components/shared/GlobalCard.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import Link from 'next/link';
 import { Button } from '~/components/ui/button';
 import { IGlobalCardType } from '~/app/types/sharedTypes';
+import { CardDescription, CardFooter, CardHeader } from '~/components/ui/card';
 
 const GlobalCard: FC<IGlobalCardType> = ({
     name,
@@ -24,21 +25,23 @@ const GlobalCard: FC<IGlobalCardType> = ({
                 <></>
             )}
             <div className='z-50 w-72 px-4 py-3'>
-                <span className='mr-3 text-lg uppercase text-gray-400'>
+                <CardHeader className='mr-3 text-lg uppercase text-gray-400 font-bold'>
                     {name}
-                </span>
+                </CardHeader>
                 {!description ? (
                     <></>
                 ) : (
-                    <p className='block truncate text-sm font-bold capitalize text-black'>
+                    <CardDescription className='block truncate text-sm font-bold capitalize text-black'>
                         {description}
-                    </p>
+                    </CardDescription>
                 )}
+                <CardFooter>
                 <div className='flex items-center p-4'>
                     <Button variant={'default'} size={'lg'}>
                         <Link href={`/${linkName}/${id}`}>Detail</Link>
                     </Button>
                 </div>
+                </CardFooter>
             </div>
         </div>
     );
