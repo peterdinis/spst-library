3340493c8ccef2cc28ab75e9e1f680ce03272608
Peter Dinis <peterdinis0@pobox.sk>
2024-03-29 08:12:01 +0100

    feat: New migration and update Publishers

---
+++ prisma/migrations/20240329070549_new_image_column_in_publisher_table/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Publisher" ADD COLUMN     "image" TEXT NOT NULL DEFAULT '';
--- src/app/_components/authors/AllAuthors.tsx
+++ src/app/_components/authors/AllAuthors.tsx
@@ -53,7 +53,7 @@ const AllAuthors: FC = () => {
                 </div>
             )}
 
-<div className='mx-auto mt-5 grid gap-8 overflow-x-auto pt-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            <div className='mx-auto mt-5 grid gap-8 overflow-x-auto pt-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
                 {filteredData &&
                     filteredData.map((filteredItem: any) => (
                         <GlobalCard
