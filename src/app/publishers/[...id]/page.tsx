1a7428e08396b44cffc9b6694df3d6b5f3f739e6
Peter Dinis <peterdinis0@pobox.sk>
2024-03-29 16:35:19 +0100

    feat: Adding detail pages for authors and publishers

---
+++ src/app/publishers/[...id]/page.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import PublisherInfo from "~/app/_components/publishers/PublisherInfo";
+
+const PublisherDetailPage: NextPage = () => {
+    return <PublisherInfo />
+}
+
+export default PublisherDetailPage;
