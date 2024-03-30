da9048278f7689639922686ef5bae54e266c5b53
Peter Dinis <peterdinis0@pobox.sk>
2024-03-30 11:03:17 +0100

    feat: Updating post select components WIP

--- src/app/_components/authors/AuthorSelect.tsx
+++ src/app/_components/authors/AuthorSelect.tsx
@@ -1,3 +1,4 @@
+import { Loader2, Ghost } from 'lucide-react';
 import { FC } from 'react';
 import {
     Select,
@@ -6,19 +7,46 @@ import {
     SelectTrigger,
     SelectValue,
 } from '~/components/ui/select';
+import { api } from '~/trpc/react';
 
 const AuthorSelect: FC = () => {
+    const { data, isLoading, isError } = api.author.fetchAuthors.useQuery();
+
+    if (isLoading) {
+        return <Loader2 className='h-8 w-8 animate-spin' />;
+    }
+
+    if (isError) {
+        return (
+            <div className='mt-6 flex justify-center align-top'>
+                <Ghost className='h-8 w-8 animate-bounce' />{' '}
+                <span className='font-bold'>
+                    Žiadny spisovatelia neboli nájdený
+                </span>
+            </div>
+        );
+    }
+
     return (
-        <Select>
-            <SelectTrigger>
-                <SelectValue placeholder='Theme' />
-            </SelectTrigger>
-            <SelectContent>
-                <SelectItem value='light'>Light</SelectItem>
-                <SelectItem value='dark'>Dark</SelectItem>
-                <SelectItem value='system'>System</SelectItem>
-            </SelectContent>
-        </Select>
+        <section className='peer mt-4 block w-full appearance-none bg-transparent px-0 py-2.5 text-lg text-gray-900 focus:outline-none focus:ring-0'>
+            <Select>
+                <SelectTrigger>
+                    <SelectValue placeholder='Výber spisovateľa/ky' />
+                </SelectTrigger>
+                <SelectContent>
+                    {data &&
+                        data.map((item) => {
+                            return (
+                                <SelectItem
+                                    value={item.name} // TODO: Premyslieť ako podľa id-čka vybrať daného autora
+                                >
+                                    {item.name}
+                                </SelectItem>
+                            );
+                        })}
+                </SelectContent>
+            </Select>
+        </section>
     );
 };
 
