8e331d526239b3b5be744470cbe33603b0f351a6
Peter Dinis <peterdinis0@pobox.sk>
2024-03-31 14:07:42 +0200

    FOO

--- src/server/lucia/actions/studentActions.ts
+++ src/server/lucia/actions/studentActions.ts
@@ -16,56 +16,56 @@ export interface ActionResponse<T> {
 export async function login(
     _: any,
     formData: FormData,
-  ): Promise<ActionResponse<LoginInput>> {
+): Promise<ActionResponse<LoginInput>> {
     const obj = Object.fromEntries(formData.entries());
-  
+
     const parsed = loginSchema.safeParse(obj);
     if (!parsed.success) {
-      const err = parsed.error.flatten();
-      return {
-        fieldError: {
-          email: err.fieldErrors.email?.[0],
-          password: err.fieldErrors.password?.[0],
-        },
-      };
+        const err = parsed.error.flatten();
+        return {
+            fieldError: {
+                email: err.fieldErrors.email?.[0],
+                password: err.fieldErrors.password?.[0],
+            },
+        };
     }
-  
+
     const { email, password } = parsed.data;
 
     const existingUser = await db.user.findFirst({
         where: {
-            email
-        }
-    })
-  
+            email,
+        },
+    });
+
     if (!existingUser) {
-      return {
-        formError: "Incorrect email or password",
-      };
+        return {
+            formError: 'Incorrect email or password',
+        };
     }
-  
+
     if (!existingUser || !existingUser?.password) {
-      return {
-        formError: "Incorrect email or password",
-      };
+        return {
+            formError: 'Incorrect email or password',
+        };
     }
-  
+
     const validPassword = await new Scrypt().verify(
-      existingUser.password,
-      password,
+        existingUser.password,
+        password,
     );
     if (!validPassword) {
-      return {
-        formError: "Incorrect email or password",
-      };
+        return {
+            formError: 'Incorrect email or password',
+        };
     }
-  
+
     const session = await lucia.createSession(String(existingUser.id), {});
     const sessionCookie = lucia.createSessionCookie(session.id);
     cookies().set(
-      sessionCookie.name,
-      sessionCookie.value,
-      sessionCookie.attributes,
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes,
     );
     return redirect(studentRedirects.afterLogin);
-  }
+}
