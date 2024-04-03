31cf3384ac7d95949ae5e43517c1e8d971eced6d
Peter Dinis <peterdinis0@pobox.sk>
2024-04-03 19:57:07 +0200

    feat: Updating student actions

--- src/server/validators/auth.ts
+++ src/server/validators/auth.ts
@@ -16,14 +16,3 @@ export const loginSchema = z.object({
 		.max(255),
 });
 export type LoginInput = z.infer<typeof loginSchema>;
-
-export const forgotPasswordSchema = z.object({
-	email: z.string().email(),
-});
-export type ForgotPasswordInput = z.infer<typeof forgotPasswordSchema>;
-
-export const resetPasswordSchema = z.object({
-	token: z.string().min(1, "Invalid token"),
-	password: z.string().min(8, "Password is too short").max(255),
-});
-export type ResetPasswordInput = z.infer<typeof resetPasswordSchema>;
