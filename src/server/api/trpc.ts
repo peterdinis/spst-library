74b20df6e86899811a923d637431f1594d0fa4b0
Peter Dinis <peterdinis0@pobox.sk>
2024-04-03 18:21:57 +0200

    feat: Protected endpoint WIP

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -9,6 +9,7 @@
 import { initTRPC, TRPCError } from "@trpc/server";
 import superjson from "superjson";
 import { ZodError } from "zod";
+import type { User } from "@prisma/client";
 
 import { db } from "~/server/db";
 
@@ -84,10 +85,21 @@ export const createTRPCRouter = t.router;
  */
 export const publicProcedure = t.procedure;
 
-export const protectedProcedure = t.procedure.use(({ ctx, next }) => {
+export const protectedProcedure = t.procedure.use(async ({ ctx, next }) => {
 	if (!ctx.db.session || !ctx.db.user) {
 	  throw new TRPCError({ code: "UNAUTHORIZED" });
+	};
+
+	const findUser = ctx.db.user.findFirst({
+		where: {
+			role: "STUDENT"
+		}
+	})
+
+	if(await findUser) {
+		throw new TRPCError({ code: "UNAUTHORIZED" });
 	}
+
 	return next({
 	  ctx: {
 		// infers the `session` and `user` as non-nullable
