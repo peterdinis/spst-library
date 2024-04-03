d699a9b9ffa1646715946db9bbd015027dff3587
Peter Dinis <peterdinis0@pobox.sk>
2024-04-03 17:36:02 +0200

    feat: Writing backend api endpoints

--- src/server/api/routers/booking.ts
+++ src/server/api/routers/booking.ts
@@ -1,3 +1,8 @@
-import { createTRPCRouter } from "../trpc";
+import { createTRPCRouter, publicProcedure } from "../trpc";
 
-export const bookingRouter = createTRPCRouter({});
+export const bookingRouter = createTRPCRouter({
+    displayAllBooking: publicProcedure.query(async({ctx}) => {
+        const allBorrowedBooks = await ctx.db.booking.findMany({});
+        return allBorrowedBooks;
+    })
+});
