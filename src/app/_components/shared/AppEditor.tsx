23acdee41b917353917181b8a8be912271397f61
Peter Dinis <peterdinis0@pobox.sk>
2024-04-05 21:39:25 +0200

    feat: Adding quill back

--- src/app/_components/shared/AppEditor.tsx
+++ src/app/_components/shared/AppEditor.tsx
@@ -1,127 +1,71 @@
-'use client'
+"use client"
 
-import { useEditor, EditorContent } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
-import TaskList from "@tiptap/extension-task-list";
-import TaskItem from "@tiptap/extension-task-item";
-import TextStyle from "@tiptap/extension-text-style";
-import { Color } from "@tiptap/extension-color";
+import { useMemo, useRef, useState } from "react";
+import QuillEditor from "react-quill";
 
-const AppEditor = () => {
-  const editor = useEditor({
-    extensions: [
-      StarterKit,
-      TextStyle,
-      Color,
-      TaskList.configure({
-        HTMLAttributes: {
-          class: "not-prose pl-2",
-        },
-      }),
-      TaskItem.configure({
-        HTMLAttributes: {
-          class: "flex items-start my-4",
-        },
-        nested: true,
-      }),
-    ],
-    content: `
-    <h2>
-      Hi there,
-    </h2>
-    <ul data-type="taskList">
-      <li data-type="taskItem" data-checked="true">A list item</li>
-      <li data-type="taskItem" data-checked="false">And another one</li>
-    </ul>
-    <p>
-      this is a basic <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-    </p>
-    <ul>
-      <li>
-        That’s a bullet list with one …
-      </li>
-      <li>
-        … or two list items.
-      </li>
-    </ul>
-    <p>
-      Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-    </p>
-<pre><code class="language-css">body {
-  display: none;
-}</code></pre>
-    <p>
-      I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-    </p>
-    <blockquote>
-      Wow, that’s amazing. Good work, boy! 👏
-      <br />
-      — Mom
-    </blockquote>
-  `,
-    editorProps: {
-      attributes: {
-        class: 'prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none',
+import "react-quill/dist/quill.snow.css";
+import { Button } from "~/components/ui/button";
+
+const Editor = () => {
+  const [value, setValue] = useState("");
+
+  const quill = useRef<any>();
+  function handler() {
+    console.log(value);
+  }
+
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          [{ header: [2, 3, 4, false] }],
+          ["bold", "italic", "underline", "blockquote"],
+          [{ color: [] }],
+          [
+            { list: "ordered" },
+            { list: "bullet" },
+            { indent: "-1" },
+            { indent: "+1" },
+          ],
+          ["link", "image"],
+          ["clean"],
+        ],
       },
-    },
-  })
+      clipboard: {
+        matchVisual: true,
+      },
+    }),
+    []
+  );
 
-  if (!editor) return null
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "color",
+    "clean",
+  ];
 
   return (
-    <div className='space-x-4'>
-      <input
-        type="color"
-        onInput={(event: any) => editor.chain().focus().setColor(event.target.value).run()}
-        value={editor.getAttributes('textStyle').color}
+    <div>
+      <QuillEditor
+        theme="snow"
+        value={value}
+        formats={formats}
+        modules={modules}
+        onChange={(value) => setValue(value)}
       />
-      <button
-        onClick={() => editor.chain().focus().setColor('#958DF1').run()}
-        className={editor.isActive('textStyle', { color: '#958DF1' }) ? 'is-active' : ''}
-      >
-        purple
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setColor('#F98181').run()}
-        className={editor.isActive('textStyle', { color: '#F98181' }) ? 'is-active' : ''}
-      >
-        red
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setColor('#FBBC88').run()}
-        className={editor.isActive('textStyle', { color: '#FBBC88' }) ? 'is-active' : ''}
-      >
-        orange
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setColor('#FAF594').run()}
-        className={editor.isActive('textStyle', { color: '#FAF594' }) ? 'is-active' : ''}
-      >
-        yellow
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setColor('#70CFF8').run()}
-        className={editor.isActive('textStyle', { color: '#70CFF8' }) ? 'is-active' : ''}
-      >
-        blue
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setColor('#94FADB').run()}
-        className={editor.isActive('textStyle', { color: '#94FADB' }) ? 'is-active' : ''}
-      >
-        teal
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setColor('#B9F18D').run()}
-        className={editor.isActive('textStyle', { color: '#B9F18D' }) ? 'is-active' : ''}
-      >
-        green
-      </button>
-      <button onClick={() => editor.chain().focus().unsetColor().run()}>unsetColor</button>
-
-      <EditorContent editor={editor} />
+      <Button variant={"ghost"} size={"lg"} onClick={handler}>Submit</Button>
     </div>
-  )
-}
+  );
+};
 
-export default AppEditor;
+export default Editor;
