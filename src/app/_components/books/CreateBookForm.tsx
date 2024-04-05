eaeb932825aff651e7d892c9bab52e162ca82e00
Peter Dinis <peterdinis0@pobox.sk>
2024-04-05 19:25:59 +0200

    Adding editor

--- src/app/_components/books/CreateBookForm.tsx
+++ src/app/_components/books/CreateBookForm.tsx
@@ -4,6 +4,7 @@ import { FC } from "react";
 import Header from "../shared/Header";
 import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
+import {Editor} from "novel"
 
 const CreateBookForm: FC = () => {
 	return (
@@ -17,9 +18,9 @@ const CreateBookForm: FC = () => {
 						placeholder="Meno Knihy"
 					/>
 				</div>
-				{/* <div className="group relative z-0 mb-6">
+				<div className="group relative z-0 mb-6">
 					<Editor />
-				</div> */}
+				</div>
 				<div className="group relative z-0 mb-6">
 					<Input
 						type="text"
