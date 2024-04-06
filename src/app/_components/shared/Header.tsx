e0d2b653313871843b084dc7bc71e71374543095
Peter Dinis <peterdinis0@pobox.sk>
2024-04-06 18:38:41 +0200

    fix: Fixing small problem

--- src/app/_components/shared/Header.tsx
+++ src/app/_components/shared/Header.tsx
@@ -3,7 +3,7 @@ import { IHeaderProps } from "~/app/types/sharedTypes";
 
 const Header: FC<IHeaderProps> = ({ text }: IHeaderProps) => {
 	return (
-		<h2 className="mt-5 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
+		<h2 className="mt-5 text-balance text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
 			{text}
 		</h2>
 	);
