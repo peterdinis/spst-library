7763217de26b679f466ac2d6bbbad4eaa6403774
Peter Dinis <peterdinis0@pobox.sk>
2024-04-12 18:58:56 +0200

    feat: Displaying error message in create category

--- src/app/_components/categories/CreateCategory.tsx
+++ src/app/_components/categories/CreateCategory.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { FC } from "react";
 import Header from "../shared/Header";
 import { Button } from "~/components/ui/button";
@@ -11,7 +9,7 @@ import { useRouter } from "next/navigation";
 const CreateCategory: FC = () => {
 	const { toast } = useToast();
 
-	const { register, handleSubmit, reset } = useForm();
+	const { register, handleSubmit, reset, formState: { errors } } = useForm(); // Pridáme errors sem
 	const router = useRouter();
 	const addCategoryMut = api.category.createCategory.useMutation({
 		onSuccess: () => {
@@ -54,18 +52,29 @@ const CreateCategory: FC = () => {
 						placeholder="Meno kategórie"
 						{...register("name", { 
 							required: true,
-							min: 5, 
+							minLength: 5, // Mala byť minLength namiesto min
 						})}
 					/>
-
+					{errors.name && errors.name.type === "required" && (
+						<span className="text-red-500">Meno kategórie je povinné</span>
+					)}
+					{errors.name && errors.name.type === "minLength" && (
+						<span className="text-red-500">Meno kategórie musí mať minimálne 5 znakov</span>
+					)}
 				</div>
 				<div className="group relative z-0 mb-6">
 					<input
 						type="text"
 						className="peer mt-4 block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-lg text-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600"
 						placeholder="Popis kategórie"
-						{...register("description", { required: true, min: 5 })}
+						{...register("description", { required: true, minLength: 5 })}
 					/>
+					{errors.description && errors.description.type === "required" && (
+						<span className="text-red-500">Popis kategórie je povinný</span>
+					)}
+					{errors.description && errors.description.type === "minLength" && (
+						<span className="text-red-500">Popis kategórie musí mať minimálne 5 znakov</span>
+					)}
 				</div>
 				<div className="flex justify-center align-top">
 					<Button variant={"default"} size={"lg"}>
