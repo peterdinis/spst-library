7dc26e1018fb7a6dbcaf36afdcea6e04a0715c5a
Peter Dinis <peterdinis0@pobox.sk>
2024-04-12 19:14:08 +0200

    feat: Adding new 404 page

---
+++ src/app/[...not_found]/page.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import GlobalErrorComponent from "../_components/shared/GlobalErrorComponent";
+
+const NotFoundPage: NextPage = () => {
+    return <GlobalErrorComponent statusCode={"404"} message={"Stránka nebola nájdená"} />
+}
+
+export default NotFoundPage;
