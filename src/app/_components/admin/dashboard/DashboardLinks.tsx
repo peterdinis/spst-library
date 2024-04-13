f7d22784412cb832a8be68f22a68474688c21c07
Peter Dinis <peterdinis0@pobox.sk>
2024-04-13 17:01:34 +0200

    infra: Formatting files

---
+++ src/app/_components/admin/dashboard/DashboardLinks.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { FC } from "react";
+
+const DashboardLinks: FC = () => {
+	return (
+		<nav className="text-sm text-gray-500 grid gap-4 dark:text-gray-400">
+			<Link
+				className="font-semibold text-gray-900 dark:text-gray-50"
+				href="/admin/books"
+			>
+				Všetky knihy
+			</Link>
+			<Link href="/admin/categories">Všetky kategŕoie</Link>
+			<Link href="/admin/publishers">Všetky vydavateľstvá</Link>
+			<Link href="/admin/authors">Všetci spisovatelia</Link>
+			<Link href="/admin/students">Všetci študenti</Link>
+			<Link href="/admin/teachers">Všetci učitelia</Link>
+			<Link href="/admin/bookings">Všetky objednávky</Link>
+		</nav>
+	);
+};
+
+export default DashboardLinks;
--- src/app/admin/profile/page.tsx
+++ src/app/admin/profile/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 import AdminProfileWrapper from "~/app/_components/admin/AdminProfileWrapper";
 import { validateRequest } from "~/server/lucia/validate-request";
-import { adminRedirects} from "~/server/utils";
+import { adminRedirects } from "~/server/utils";
 
 export default async function ProfilePage() {
 	const { user } = await validateRequest();
