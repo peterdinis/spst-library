6fae5045608751b8cb775e21c9da7c9d3cc246a8
Peter Dinis <peterdinis0@pobox.sk>
2024-04-13 17:33:52 +0200

    feat: Finish implmenting new admin pages

---
+++ src/app/admin/bookings/page.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import AdminAllBorrowedBooks from "~/app/_components/admin/AdminAllBorrowedBooks";
+
+const AdminBookingsPage: NextPage = () => {
+    return <AdminAllBorrowedBooks />
+}
+
+export default AdminBookingsPage;
---
+++ src/app/admin/publishers/page.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import AdminAllPublishers from "~/app/_components/admin/AdminAllPublishers";
+
+const AdminPublishersPage: NextPage = () => {
+    return <AdminAllPublishers />
+}
+
+export default AdminPublishersPage
