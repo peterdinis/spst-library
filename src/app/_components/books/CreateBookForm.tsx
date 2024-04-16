18c11ec8f236371296bf76aaaacf053a5bcfde0d
Peter Dinis <peterdinis0@pobox.sk>
2024-04-16 18:57:05 +0200

    FOO

--- src/app/_components/books/CreateBookForm.tsx
+++ src/app/_components/books/CreateBookForm.tsx
@@ -6,90 +6,143 @@ import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
 import dynamic from "next/dynamic";
 import AuthorSelect from "../authors/AuthorSelect";
+import {useForm, FieldValues} from "react-hook-form";
 import CategorySelect from "../categories/CategorySelect";
-import { Tooltip } from "~/components/ui/tooltip";
 import {
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@radix-ui/react-tooltip";
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from "~/components/ui/tooltip";
+import { useToast } from "~/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import { api } from "~/trpc/react";
 
 const AppEditor = dynamic(() => import("../shared/AppEditor"), { ssr: false });
 
 const CreateBookForm: FC = () => {
-  return (
-    <>
-      <Header text="Tvorba novej knihy" />
-      <form className="mx-auto mt-10 max-w-2xl">
-        <div className="group relative z-0 mb-6">
-          <Input
-            type="text"
-            className="peer mt-4 block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-lg text-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600"
-            placeholder="Meno Knihy"
-          />
-        </div>
-        <div className="group relative z-0 mb-6">
-          <Input
-            type="text"
-            className="peer mt-4 block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-lg text-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600"
-            placeholder="Obrázok"
-          />
-        </div>
-        <div className="group relative z-0 mb-6">
-          <AuthorSelect />
-        </div>
-        <div className="group relative z-0 mb-6">
-          <Input
-            type="text"
-            className="peer mt-4 block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-lg text-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600"
-            placeholder="Rok"
-          />
-        </div>
-        <div className="group relative z-0 mb-6">
-          <Input
-            type="text"
-            className="peer mt-4 block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-lg text-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600"
-            placeholder="Počet strán"
-          />
-        </div>
-        <div className="group relative z-0 mb-6">
-          <Input
-            type="text"
-            className="peer mt-4 block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-lg text-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600"
-            placeholder="Dostupná"
-          />
-        </div>
-        <div className="group relative z-0 mb-6">
-          <Input
-            type="text"
-            className="peer mt-4 block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-lg text-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600"
-            placeholder="Počet kusov"
-          />
-        </div>
-        <div className="group relative z-0 mb-6">
-          <CategorySelect />
-        </div>
+	const { toast } = useToast();
 
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger>Krátke info</TooltipTrigger>
-            <TooltipContent>
-              <p className="font-bold text-gray-800 p-2 text-lg">V tomto editore môžte napísať krátke informácie o knihe. Max 200 riadkov</p>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <div className="group relative z-0 mb-6">
-          <AppEditor />
-        </div>
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors },
+	} = useForm(); 
+	const router = useRouter();
 
-        <div className="flex justify-center align-top">
-          <Button variant={"default"} size={"lg"}>
-            Pridaj novú knihu
-          </Button>
-        </div>
-      </form>
-    </>
-  );
+	const addBookMut = api.book.createNewBook.useMutation({
+		onSuccess: () => {
+			toast({
+				title: "Nová kniha bola vytvorená",
+				duration: 2000,
+				className: "bg-green-500 text-white",
+			});
+			router.push("/books");
+		},
+
+		onError: () => {
+			toast({
+				title: "Nová kniha nebola vytvorená",
+				duration: 2000,
+				className: "bg-red-500 text-white",
+			});
+		}
+
+	})
+
+	const onSubmit = async (data: FieldValues) => {
+		await addBookMut.mutateAsync({
+			name: data.name,
+			description: data.description,
+			image: data.image,
+			year: data.year,
+			pages: data.pages,
+			isAvaiable: data.isAvaiable,
+			itemsInStock: data.itemsInStock,
+			categoryId: data.categoryId,
+			authorId: data.authorId,
+			publisherId: data.publisherId,
+		});
+		reset();
+	};
+
+	return (
+		<>
+			<Header text="Tvorba novej knihy" />
+			<form className="mx-auto mt-10 max-w-2xl">
+				<div className="group relative z-0 mb-6">
+					<Input
+						type="text"
+						className="peer mt-4 block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-lg text-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600"
+						placeholder="Meno Knihy"
+					/>
+				</div>
+				<div className="group relative z-0 mb-6">
+					<Input
+						type="text"
+						className="peer mt-4 block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-lg text-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600"
+						placeholder="Obrázok"
+					/>
+				</div>
+				<div className="group relative z-0 mb-6">
+					<AuthorSelect />
+				</div>
+				<div className="group relative z-0 mb-6">
+					<Input
+						type="text"
+						className="peer mt-4 block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-lg text-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600"
+						placeholder="Rok"
+					/>
+				</div>
+				<div className="group relative z-0 mb-6">
+					<Input
+						type="text"
+						className="peer mt-4 block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-lg text-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600"
+						placeholder="Počet strán"
+					/>
+				</div>
+				<div className="group relative z-0 mb-6">
+					<Input
+						type="text"
+						className="peer mt-4 block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-lg text-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600"
+						placeholder="Dostupná"
+					/>
+				</div>
+				<div className="group relative z-0 mb-6">
+					<Input
+						type="text"
+						className="peer mt-4 block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-lg text-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600"
+						placeholder="Počet kusov"
+					/>
+				</div>
+				<div className="group relative z-0 mb-6">
+					<CategorySelect />
+				</div>
+
+				<TooltipProvider>
+					<Tooltip>
+						<TooltipTrigger>Krátke info</TooltipTrigger>
+						<TooltipContent>
+							<p className="font-bold text-gray-800 p-2 text-lg">
+								V tomto editore môžte napísať krátke informácie
+								o knihe. Max 200 riadkov
+							</p>
+						</TooltipContent>
+					</Tooltip>
+				</TooltipProvider>
+				<div className="group relative z-0 mb-6">
+					<AppEditor />
+				</div>
+
+				<div className="flex justify-center align-top">
+					<Button variant={"default"} size={"lg"}>
+						Pridaj novú knihu
+					</Button>
+				</div>
+			</form>
+		</>
+	);
 };
 
 export default CreateBookForm;
