40d91acc750d424693e4f09b6701cad9b833690e
Peter Dinis <peterdinis0@pobox.sk>
2024-04-20 12:14:38 +0200

    style: Updating admin part

--- src/app/_components/admin/AdminProfileWrapper.tsx
+++ src/app/_components/admin/AdminProfileWrapper.tsx
@@ -23,7 +23,7 @@ const AdminProfileWrapper: FC<IProfileWrapperProps> = ({
 		<>
 			<Header text="Admin časť" />
 			<div className="flex flex-col min-h-screen w-full">
-				<main className="flex min-h-[calc(100vh_-_theme(spacing.16))] bg-gray-100/40 flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10 dark:bg-gray-800/40">
+				<main className="flex min-h-[calc(100vh_-_theme(spacing.16))]  flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10">
 					<div className="grid md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr] items-start gap-6 max-w-6xl w-full mx-auto">
 						<DashboardLinks />
 						<div className="grid gap-6">
