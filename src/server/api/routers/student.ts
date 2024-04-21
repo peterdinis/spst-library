f918f7d4fc6a16f4a464321e94a8f125acde3266
Peter Dinis <peterdinis0@pobox.sk>
2024-04-21 09:20:02 +0200

    feat: Adding two new endpoints

--- src/server/api/routers/student.ts
+++ src/server/api/routers/student.ts
@@ -1,3 +1,14 @@
-import { createTRPCRouter } from "../trpc";
+import { STUDENT } from "~/server/lucia/constants";
+import { createTRPCRouter, publicProcedure } from "../trpc";
 
-export const studentRouter = createTRPCRouter({});
+export const studentRouter = createTRPCRouter({
+    fetchAllStudents: publicProcedure.query(async({ctx}) => {
+        const allStudents = await ctx.db.user.findMany({
+            where: {
+                role: STUDENT
+            }
+        });
+
+        return allStudents;
+    })
+});
--- src/server/api/routers/teacher.ts
+++ src/server/api/routers/teacher.ts
@@ -1,3 +1,14 @@
-import { createTRPCRouter } from "../trpc";
+import {TEACHER } from "~/server/lucia/constants";
+import { createTRPCRouter, publicProcedure } from "../trpc";
 
-export const teacherRouter = createTRPCRouter({});
+export const teacherRouter = createTRPCRouter({
+    fetchAllTeachers: publicProcedure.query(async({ctx}) => {
+        const allTeachers = await ctx.db.user.findMany({
+            where: {
+                role: TEACHER
+            }
+        });
+
+        return allTeachers;
+    })
+});
