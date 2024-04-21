285144e10ddebe5be6b9e859c634d9bff181ca8c
Peter Dinis <peterdinis0@pobox.sk>
2024-04-21 13:00:22 +0200

    feat: Updating sheets fot update/delete

--- src/app/_components/shared/SheetHelper.tsx
+++ src/app/_components/shared/SheetHelper.tsx
@@ -1,30 +1,39 @@
-import { Sheet } from "lucide-react";
+"use client"
+
 import { FC, ReactNode } from "react";
+import { Button } from "~/components/ui/button";
 import {
-	SheetTrigger,
+	Sheet,
 	SheetContent,
 	SheetHeader,
 	SheetTitle,
 	SheetDescription,
+	SheetTrigger,
 } from "~/components/ui/sheet";
+
+type BtnVariants = "default" | "destructive" | "outline" | "secondary" | "ghost" | "link";
 
 interface ISheetHelperProps {
 	children?: ReactNode;
 	title: string;
 	secondTitle: string;
+	variantProp: BtnVariants;
 }
 
 const SheetHelper: FC<ISheetHelperProps> = ({
 	children,
 	title,
 	secondTitle,
+	variantProp
 }) => {
 	return (
 		<Sheet>
-			<SheetTrigger>{title}</SheetTrigger>
+			<SheetTrigger>
+				<Button variant={variantProp} size="lg">{title}</Button>
+			</SheetTrigger>
 			<SheetContent>
 				<SheetHeader>
-					<SheetTitle>{secondTitle}</SheetTitle>
+					<SheetTitle className="text-2xl p-5 font-bold">{secondTitle}</SheetTitle>
 					<SheetDescription>
                         {children}
 					</SheetDescription>
