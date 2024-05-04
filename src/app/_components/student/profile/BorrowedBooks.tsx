56ac321975ce803d02213a2d990f0ceedc9dcf26
Peter Dinis <peterdinis0@pobox.sk>
2024-05-04 12:10:40 +0200

    feat: Implementing pagination for borrower books

--- src/app/_components/student/profile/BorrowedBooks.tsx
+++ src/app/_components/student/profile/BorrowedBooks.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Loader2 } from "lucide-react";
 import { FC, useState } from "react";
@@ -14,32 +14,33 @@ import { IBorrowedBookingTypes } from "~/app/types/bookingTypes";
 import GlobalPagination from "../../shared/GlobalPagination";
 
 const BorrowedBooks: FC = () => {
-	const [page, setPage] = useState(0);
+  const [page, setPage] = useState(0);
 
-	const limit = 10 as const;
+  const limit = 6 as const;
   const studentCookie = useStudentCookie();
 
   const {
-		data: paginatedData,
-		fetchNextPage,
-		isFetchingNextPage,
-		isLoading: paginatedLoading,
-		isError: paginatedError,
-	} = api.booking.paginatedBoooking.useInfiniteQuery(
-		{
-			limit,
-			userEmail: studentCookie?.email
-		},
-		{
-			
-		}
-	);
+    data: paginatedData,
+    fetchNextPage,
+    isFetchingNextPage,
+    isLoading: paginatedLoading,
+    isError: paginatedError,
+  } = api.booking.paginatedBoooking.useInfiniteQuery(
+    {
+		limit,
+		userEmail: studentCookie?.email
+    },
+    {
+      //@ts-ignore
+      getNextPageParam: (lastPage: { nextCursor: any }) => lastPage.nextCursor,
+    }
+  );
 
-  if (isLoading) {
-    return <Loader2 className="animate-bounce w-8 h-8" />;
+  if (isFetchingNextPage || paginatedLoading) {
+    return <Loader2 className="h-8 w-8 animate-spin" />;
   }
 
-  if (isError) {
+  if (paginatedError) {
     return (
       <GlobalErrorComponent
         message="Nepodarilo sa načítať požičané knihy"
@@ -47,6 +48,20 @@ const BorrowedBooks: FC = () => {
       />
     );
   }
+
+  const toShow = paginatedData?.pages[page]?.items;
+  const nextCursor = paginatedData?.pages[page]?.nextCursor;
+
+  const handleFetchNextPage = async () => {
+    await fetchNextPage();
+    setPage((prev) => prev + 1);
+  };
+
+  const handleFetchPreviousPage = () => {
+    setPage((prev) => prev - 1);
+  };
+
+  console.log("To show", toShow);
 
   return (
     <div className="space-y-6">
@@ -56,8 +71,9 @@ const BorrowedBooks: FC = () => {
         </CardHeader>
         <CardContent>
           <div className="grid gap-4">
-            {data &&
-              data.map((item: IBorrowedBookingTypes) => {
+            {!paginatedLoading &&
+              toShow &&
+              toShow.map((item: IBorrowedBookingTypes) => {
                 return (
                   <>
                     <div className="flex items-center space-x-4">
@@ -81,20 +97,12 @@ const BorrowedBooks: FC = () => {
                         <ExtendedBookingModal />
                       </Button>
                     </div>
-					<GlobalPagination
-                          page={0}
-                          nextCursor={0}
-                          handleFetchPreviousPage={function (
-                            ...args: unknown[]
-                          ): void {
-                            throw new Error("Function not implemented.");
-                          }}
-                          handleFetchNextPage={function (
-                            ...args: unknown[]
-                          ): void {
-                            throw new Error("Function not implemented.");
-                          }}
-                        />
+                    <GlobalPagination
+                      handleFetchNextPage={handleFetchNextPage}
+                      page={page}
+                      nextCursor={nextCursor as unknown as number}
+                      handleFetchPreviousPage={handleFetchPreviousPage}
+                    />
                   </>
                 );
               })}
