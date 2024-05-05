6500991b384f49ae100033a0ed3fa02213641e01
Peter Dinis <peterdinis0@pobox.sk>
2024-05-05 10:30:27 +0200

    FOO

--- src/app/_components/admin/AdminAllTeachers.tsx
+++ src/app/_components/admin/AdminAllTeachers.tsx
@@ -2,15 +2,20 @@
 
 import { Loader2 } from "lucide-react";
 import { FC } from "react";
-import { api } from "~/trpc/react";
+import axios from "axios";
 import GlobalErrorComponent from "../shared/GlobalErrorComponent";
 import Header from "../shared/Header";
 import { DataTable } from "../shared/GlobalTable";
-import { columns } from "./columns/bookColumns";
+import { columns } from "./columns/studentColumns";
+import { useQuery } from "@tanstack/react-query";
 
 const AdminAllTeachers: FC = () => {
-	const { data, isLoading, isError } =
-		api.teacher.fetchAllTeachers.useQuery(); /* TODO: Call external server later */
+	const { data, isLoading, isError } = useQuery({
+		queryKey: ["allTeachers"],
+		queryFn: () => {
+			return axios.get(process.env.NEXT_PUBLIC_AUTH_API as unknown as string + "auth/users/teachers")
+		}
+	})
 
 	if (isLoading) {
 		return <Loader2 className="animate-bounce w-8 h-8" />;
@@ -30,7 +35,7 @@ const AdminAllTeachers: FC = () => {
 	return (
 		<div className="mt-4">
 			<Header text="Zoznam všetkých učiteľov" />
-			<DataTable columns={columns} data={data} />
+			<DataTable columns={columns} data={data as any} />
 		</div>
 	);
 };
