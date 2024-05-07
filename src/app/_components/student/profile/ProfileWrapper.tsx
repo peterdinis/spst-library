9fc27fc44f460b094c2d6b205ef9f5837fa900aa
Peter Dinis <peterdinis0@pobox.sk>
2024-05-07 18:38:52 +0200

    FOO

--- src/app/_components/student/profile/ProfileWrapper.tsx
+++ src/app/_components/student/profile/ProfileWrapper.tsx
@@ -1,21 +1,14 @@
 "use client";
 
-import { FC, useEffect } from "react";
+import { FC} from "react";
 import BorrowedBooks from "./BorrowedBooks";
 import Settings from "./Settings";
 import useStudentCookie from "~/hooks/useStudentCookie";
 import { ICookieAuthType } from "~/app/types/authTypes";
-import { useRouter } from "next/navigation";
 
 const ProfileWrapper: FC = () => {
 	const studentCookie = useStudentCookie();
-	const router = useRouter();
 
-	useEffect(() => {
-		if (!studentCookie) {
-			router.push("/student/login");
-		}
-	}, [studentCookie]);
 	return (
 		<div className="grid md:grid-cols-2 md:gap-6 ml-4 mr-4">
 			<div className="space-y-6">
