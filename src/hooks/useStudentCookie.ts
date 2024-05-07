417d0b4c80190c37ce3dc2af1adcec9970757e91
Peter Dinis <peterdinis0@pobox.sk>
2024-05-07 18:32:14 +0200

    infra: Update use student hook

--- src/hooks/useStudentCookie.ts
+++ src/hooks/useStudentCookie.ts
@@ -4,32 +4,39 @@ import { useState, useEffect } from "react";
 import Cookie from "js-cookie";
 import { ICookieAuthType } from "~/app/types/authTypes";
 
-const useStudentCookie = (): ICookieAuthType | null => {
-	const [studentCookie, setStudentCookie] = useState<ICookieAuthType | null>(
-		null,
-	);
+const useStudentCookie = (): {
+  studentCookie: ICookieAuthType | null;
+  isCookieLoaded: boolean;
+} => {
+  const [studentCookie, setStudentCookie] = useState<ICookieAuthType | null>(
+    null
+  );
+  const [isCookieLoaded, setIsCookieLoaded] = useState<boolean>(false);
 
-	useEffect(() => {
-		const fetchStudentCookie = () => {
-			try {
-				const cookieValue = Cookie.get("studentD");
-				if (cookieValue) {
-					const parsedCookie = JSON.parse(
-						cookieValue,
-					) as ICookieAuthType;
-					setStudentCookie(parsedCookie);
-				}
-			} catch (error) {
-				console.error("Error parsing student cookie:", error);
-			}
-		};
+  useEffect(() => {
+    const fetchStudentCookie = () => {
+      try {
+        const cookieValue = Cookie.get("studentD");
+        if (cookieValue) {
+          const parsedCookie = JSON.parse(cookieValue) as ICookieAuthType;
+          setStudentCookie(parsedCookie);
+          setIsCookieLoaded(true);
+        } else {
+          setStudentCookie(null);
+          setIsCookieLoaded(true);
+        }
+      } catch (error) {
+        console.error("Error parsing student cookie:", error);
+        setStudentCookie(null);
+        setIsCookieLoaded(true);
+      }
+    };
 
-		fetchStudentCookie();
+    fetchStudentCookie();
+    return () => {};
+  }, []);
 
-		return () => {};
-	}, []);
-
-	return studentCookie;
+  return { studentCookie, isCookieLoaded };
 };
 
 export default useStudentCookie;
