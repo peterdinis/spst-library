cb1caeb42fcdb8006659df19759efc765965bd41
Peter Dinis <peterdinis0@pobox.sk>
2024-05-10 19:03:56 +0200

    feat: Writing settings page

--- src/app/_components/admin/settings/AdminSettingsWrapper.tsx
+++ src/app/_components/admin/settings/AdminSettingsWrapper.tsx
@@ -1,10 +1,14 @@
 import { FC } from "react";
 import Header from "../../shared/Header";
+import SettingsWrapper from "./SettingsWrapper";
 
 const AdminSettingsWrapper: FC = () => {
     return (
         <>
             <Header text="Iné nastavenia" />
+            <div className="mt-4">
+                <SettingsWrapper />
+            </div>
         </>
     )
 }
