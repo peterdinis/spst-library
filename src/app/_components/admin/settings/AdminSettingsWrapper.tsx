689f54c9a6a66a8028d8ca211fc0570b8c2ca203
Peter Dinis <peterdinis0@pobox.sk>
2024-05-10 18:58:33 +0200

    feat: Writing admin settings page

---
+++ src/app/_components/admin/settings/AdminSettingsWrapper.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import Header from "../../shared/Header";
+
+const AdminSettingsWrapper: FC = () => {
+    return (
+        <>
+            <Header text="Iné nastavenia" />
+        </>
+    )
+}
+
+export default AdminSettingsWrapper
