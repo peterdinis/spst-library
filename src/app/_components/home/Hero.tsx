93202f89a2c314c101da18dcc5a3886b026d67b5
Peter Dinis <peterdinis0@pobox.sk>
2024-05-11 16:06:31 +0200

    feat: Socket itergration react

--- src/app/_components/home/Hero.tsx
+++ src/app/_components/home/Hero.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import schollImage from "../../../../public/img/main.png";
 import { Button } from "~/components/ui/button";
 import { motion } from "framer-motion";
+import io from 'socket.io-client';
 
 const Hero: FC = () => {
+  const [connectionStatus, setConnectionStatus] = useState<string>('Connecting...');
+  const [socket, setSocket] = useState<any>();
+
   const homepageVariants = {
     hidden: { opacity: 0, scale: 0.9 },
     visible: {
@@ -19,11 +23,41 @@ const Hero: FC = () => {
     },
   };
 
+  useEffect(() => {
+    const socket = io('ws://localhost:4000'); // Upravte URL na adresu vašej WebSocket servera
+
+    socket.on('connect', () => {
+      console.log('Connected to WebSocket server');
+      setConnectionStatus('Connected');
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Disconnected from WebSocket server');
+      setConnectionStatus('Disconnected');
+    });
+
+    setSocket(socket);
+
+    return () => {
+      if (socket) {
+        socket.disconnect();
+      }
+    };
+  }, []); // Zavolá sa len pri prvom vykreslení komponentu
+
+  const handleSendData = () => {
+    if (socket) {
+      socket.emit('custom-event', 'Hello from client!'); // Vytvorte a pošlite vlastnú udalosť na server
+    }
+  };
+
   return (
     <motion.div
       variants={homepageVariants}
       className="container relative z-0 mx-auto px-4 xl:px-0"
     >
+      <h2>WebSocket Connection Status: {connectionStatus}</h2>
+      <button onClick={handleSendData}>Send Data</button>
       <div className="flex flex-col-reverse md:flex-row">
         <div className="md:w-3/5 md:pt-24 lg:py-32">
           <h1 className="text-heading-color dark:text-blue-50  text-center text-3xl font-black leading-tight tracking-tighter text-gray-900 md:w-7/12 md:text-left lg:text-6xl xl:text-8xl">
