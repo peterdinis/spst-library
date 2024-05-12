f2f79ca678205df169669a10adf4062757b1b0b9
Peter Dinis <peterdinis0@pobox.sk>
2024-05-12 12:41:29 +0200

    fix: Fixing small bugs in CreateBookForm

--- src/app/_components/books/CreateBookForm.tsx
+++ src/app/_components/books/CreateBookForm.tsx
@@ -137,9 +137,9 @@ const CreateBookForm: FC = () => {
             type="checkbox"
             className="peer dark:text-blue-50 mt-4 block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-lg text-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600"
             placeholder="Dostupná"
-			{...register("isAvaiable", {
-				required: true
-			})}
+            {...register("isAvaiable", {
+              required: true,
+            })}
           />
         </div>
         <div className="group relative z-0 mb-6">
@@ -147,10 +147,10 @@ const CreateBookForm: FC = () => {
             type="text"
             className="peer dark:text-blue-50 mt-4 block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-lg text-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600"
             placeholder="Počet kusov"
-			{...register("isAvaiable", {
-				required: true,
-				valueAsNumber: true
-			})}
+            {...register("itemsInStock", {
+              required: true,
+              valueAsNumber: true,
+            })}
           />
         </div>
         <div className="group relative z-0 mb-6">
@@ -182,7 +182,7 @@ const CreateBookForm: FC = () => {
         </div>
 
         <div className="group relative z-0 mb-6">
-		<Controller
+          <Controller
             name="description"
             control={control}
             defaultValue=""
