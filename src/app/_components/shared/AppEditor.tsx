21ed5858ea3865d04c6cb5798a381f68c33eb0ba
Peter Dinis <peterdinis0@pobox.sk>
2024-05-12 12:59:07 +0200

    fix: Fixing small bug in AppEditor

--- src/app/_components/shared/AppEditor.tsx
+++ src/app/_components/shared/AppEditor.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useMemo } from "react";
 import QuillEditor from "react-quill";
@@ -10,35 +10,54 @@ interface AppEditorProps {
 }
 
 const AppEditor = ({ value, onChange }: AppEditorProps) => {
-	const modules = useMemo(() => ({
-		toolbar: {
-			container: [
-				[{ header: [1, 2, 3, 4, false] }],
-				["bold", "italic", "underline", "blockquote"],
-				[{ color: [] }],
-				[{ list: "ordered" }, { list: "bullet" }, { indent: "-1" }, { indent: "+1" }],
-				["link", "image"],
-				["clean"],
-			],
-		},
-		clipboard: { matchVisual: true },
-		history: { delay: 2000, maxStack: 500, userOnly: true },
-	}), []);
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          [{ header: [1, 2, 3, 4, false] }],
+          ["bold", "italic", "underline", "blockquote"],
+          [{ color: [] }],
+          [
+            { list: "ordered" },
+            { list: "bullet" },
+            { indent: "-1" },
+            { indent: "+1" },
+          ],
+          ["link", "image"],
+          ["clean"],
+        ],
+      },
+      clipboard: { matchVisual: true },
+      history: { delay: 2000, maxStack: 500, userOnly: true },
+    }),
+    []
+  );
 
-	const formats = [
-		"header", "bold", "italic", "underline", "strike", "blockquote",
-		"list", "bullet", "indent", "link", "image", "color", "clean"
-	];
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "color",
+    "clean",
+  ];
 
-	return (
-		<QuillEditor
-			theme="snow"
-			value={value}
-			formats={formats}
-			modules={modules}
-			onChange={(content, delta, source, editor) => onChange(editor.getHTML())}
-		/>
-	);
+  return (
+    <QuillEditor
+      theme="snow"
+      value={value}
+      formats={formats}
+      modules={modules}
+      onChange={(content, delta, source, editor) => onChange(editor.getText())}
+    />
+  );
 };
 
 export default AppEditor;
