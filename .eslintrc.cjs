536d3fc4c8ddf4b589064f025ca854ea0670360b
Peter Dinis <peterdinis0@pobox.sk>
2024-05-13 18:20:08 +0200

    FOO

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,18 +15,13 @@ const config = {
 	],
 	rules: {
 		"@typescript-eslint/array-type": "off",
+		'@typescript-eslint/no-unused-vars': ['error', { 'varsIgnorePattern': '^_', 'argsIgnorePattern': '^_' }],
 		"@typescript-eslint/consistent-type-definitions": "off",
 		"@typescript-eslint/consistent-type-imports": [
 			"warn",
 			{
 				prefer: "type-imports",
 				fixStyle: "inline-type-imports",
-			},
-		],
-		"@typescript-eslint/no-unused-vars": [
-			"warn",
-			{
-				argsIgnorePattern: "^_",
 			},
 		],
 		"@typescript-eslint/require-await": "off",
