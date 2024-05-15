b8cba754c307e027ece432584024d8aba2af5bb8
Peter Dinis <peterdinis0@pobox.sk>
2024-05-15 18:36:29 +0200

    FOO

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,7 +4,7 @@ import { NextPage } from "next";
 import { useState, useEffect } from "react";
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:4000', {
+const socket = io(process.env.NEXT_PUBLIC_URL as unknown as string, {
     transports: ["websockets"]
 });  // Make sure this URL matches the server
 
