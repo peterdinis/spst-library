851cd930b183a5938033f7f06f2244e41c52c2a0
Peter Dinis <peterdinis0@pobox.sk>
2024-05-16 19:25:18 +0200

    feat: Infra Move styles to new file

--- src/app/_components/pdfs/PdfWrapper.tsx
+++ src/app/_components/pdfs/PdfWrapper.tsx
@@ -1,21 +1,10 @@
 import React, { FC } from "react";
-import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+import { Page, Text, View, Document } from "@react-pdf/renderer";
+import { styles } from "./styles";
 
 type PdfWrapperProps = {
   data: any[];
 };
-
-const styles = StyleSheet.create({
-  page: {
-    flexDirection: "row",
-    backgroundColor: "#E4E4E4",
-  },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
-  },
-});
 
 const PdfWrapper: FC<PdfWrapperProps> = ({ data }) => (
   <Document>
