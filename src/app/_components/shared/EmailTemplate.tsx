9e3c22953934649c9e98042b2915d67972588caf
Peter Dinis <peterdinis0@pobox.sk>
2024-05-18 13:04:56 +0200

    feat: New router file was created

--- src/app/_components/shared/EmailTemplate.tsx
+++ src/app/_components/shared/EmailTemplate.tsx
@@ -11,13 +11,13 @@ import {
   } from '@react-email/components';
   import * as React from 'react';
   
-  type MessageUsEmailProps = {
-    name: string;
-    message: string;
+  type EmailTemplateProps = {
+    email: string;
+    subject: string;
   };
   
-  const MessageUsEmail = ({ name, message }: MessageUsEmailProps) => {
-    const previewText = `Weekly Updates 🚀${name} sent you a message.`;
+  const EmailTemplate = ({ email, subject }: EmailTemplateProps) => {
+    const previewText = `Nový email pre 🚀${email}.`;
   
     return (
       <Html>
@@ -27,20 +27,15 @@ import {
           <Body className='bg-white my-auto mx-auto font-sans'>
             <Container className='my-[20px] mx-auto p-[20px] max-w-4xl'>
               <Heading className='text-black text-[20px] font-normal text-left'>
-                <strong>Hello {name},</strong>
+                <strong>Ahoj {email},</strong>
               </Heading>
               <Text className='text-black text-[14px] leading-[24px]'>
-                {message}
+                {subject}
               </Text>
   
               <Hr className='my-[16px] mx-0 w-full' />
-              <Text className='text-[#666666] text-[12px]'>
-               In a world where every email matters, Resend empowers you to send emails that captivate, engage, and convert. It's more than just an email platform; it's a catalyst for modernizing your email communication.
-  Don't settle for outdated email practices. Embrace the future of email communication with Resend and watch your messages soar to new heights.
-              </Text>
-              <Text className='text-[#666666] text-[12px]'>
-                This email is delivered to you through the Resend SDK
-                integrations.✨
+              <Text className='text-black text-[14px] leading-[24px]'>
+                admin@spstkniznica.sk
               </Text>
             </Container>
           </Body>
@@ -49,4 +44,4 @@ import {
     );
   };
   
-  export default MessageUsEmail;
+  export default EmailTemplate;
