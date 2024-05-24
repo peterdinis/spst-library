ae6ae7e9da425aa4d1f0da8538eda7550327b7b4
Peter Dinis <peterdinis0@pobox.sk>
2024-05-24 20:17:42 +0200

    feat: Small layout update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={`font-sans ${inter.variable}`}>
+			<body className={`font-sans  ${inter.variable}`}>
 				<TRPCReactProvider>
 					<ThemeProvider attribute="class">
 						<Navigation />
