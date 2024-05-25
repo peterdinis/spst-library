2103396926f7ca04f3da1f541742dbc3330c26c5
Peter Dinis <peterdinis0@pobox.sk>
2024-05-25 16:21:44 +0200

    feat: Update sheets logic

--- src/app/_components/books/BookSheets.tsx
+++ src/app/_components/books/BookSheets.tsx
@@ -5,147 +5,156 @@ import { Button } from "~/components/ui/button";
 import SheetHelper from "../shared/SheetHelper";
 import { Input } from "~/components/ui/input";
 import { Book } from "@prisma/client";
-import { Label } from "~/components/ui/label";
 import { api } from "~/trpc/react";
 import { useToast } from "~/components/ui/use-toast";
 import { useRouter } from "next/navigation";
-import { FieldValues, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 interface IBookSheetsProps {
-	name: string;
-	data: Book;
+    name: string;
+    data: Book;
 }
 
 const BookSheets: FC<IBookSheetsProps> = ({ name, data }: IBookSheetsProps) => {
-	const { toast } = useToast();
-	const router = useRouter();
-	const deleteBookMut = api.book.deleteBook.useMutation();
-	const updateBookMut = api.book.updateBook.useMutation({
-		onSuccess: () => {
-			toast({
-				title: "Kniha bola úpravená",
-				duration: 2000,
-				className: "bg-green-500 text-white",
-			});
-			router.push(`/books/${data?.id!}`);
-		},
+    const { toast } = useToast();
+    const router = useRouter();
+    const deleteBookMut = api.book.deleteBook.useMutation();
+    const updateBookMut = api.book.updateBook.useMutation();
 
-		onError: () => {
-			toast({
-				title: "Kniha nebola úpravená",
-				duration: 2000,
-				className: "bg-red-500 text-white",
-			});
-		},
-	});
+    const deleteBookFn = async (e: FormEvent) => {
+        e.preventDefault();
 
-	const deleteBookFn = async (e: FormEvent) => {
-		e.preventDefault();
+        await deleteBookMut.mutateAsync({
+            id: data?.id,
+        });
 
-		await deleteBookMut.mutateAsync({
-			id: data?.id,
-		});
+        toast({
+            title: "Book was successfully deleted",
+            duration: 2000,
+            className: "bg-green-500",
+        });
+        router.push("/books");
+    };
 
-		toast({
-			title: "Kniha bola úspešené zmazaná",
-			duration: 2000,
-			className: "bg-green-500",
-		});
-		router.push("/books");
-	};
+    const { register, handleSubmit } = useForm();
 
-	const {
-		register,
-		handleSubmit,
-		reset,
-		formState: { errors },
-	} = useForm();
+    const onSubmit = async (formData: any) => {
+        try {
+            await updateBookMut.mutateAsync({
+                id: data.id,
+                name: formData.name,
+                description: formData.description,
+                image: formData.image,
+                year: formData.year,
+                isAvaiable: formData.isAvaiable,
+                itemsInStock: formData.itemsInStock,
+            });
 
-	const onSubmit = async (data: FieldValues) => {
-		// TODO: Musíme ešte sa zamyslieť čo budeme pri knihe upravovať
-	};
+            toast({
+                title: "Book was successfully updated",
+                duration: 2000,
+                className: "bg-green-500",
+            });
+            router.push(`/books/${data?.id}`);
+        } catch (error) {
+            toast({
+                title: "Failed to update book",
+                duration: 2000,
+                className: "bg-red-500",
+            });
+        }
+    };
 
-	return (
-		<div className="flex mt-5">
-			<SheetHelper
-				variantProp="default"
-				title={"Upraviť knihu"}
-				secondTitle={"Upraviť knihu"}
-			>
-				<span className="mt-2 font-bold text-xl">
-					Uprava knihy: {name}
-				</span>
-				<div>
-					<form>
-						<Input
-							type="text"
-							className="mt-5"
-							placeholder="Meno"
-							value={data?.name}
-						/>
-						<Input
-							type="text"
-							className="mt-5"
-							placeholder="Popis"
-							value={data?.description}
-						/>
-						<Input
-							type="text"
-							className="mt-5"
-							placeholder="Obrázok"
-							value={data?.image}
-						/>
-						<Input
-							type="text"
-							className="mt-5"
-							placeholder="Rok"
-							value={data?.year}
-						/>
-						<div className="mt-5">
-							<Label>Dostupnosť Knihy</Label>
-							<Input
-								type="checkbox"
-								className="mt-5"
-								value={data?.isAvaiable as unknown as string}
-							/>
-						</div>
-						<Input
-							type="text"
-							className="mt-5"
-							placeholder="Počet kusov"
-							value={data?.itemsInStock}
-						/>
+    return (
+        <div className="flex mt-5">
+            <SheetHelper
+                variantProp="default"
+                title={"Update Book"}
+                secondTitle={"Update Book"}
+            >
+                <span className="mt-2 font-bold text-xl">
+                    Update Book: {name}
+                </span>
+                <div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Input
+                            type="text"
+                            className="mt-5"
+                            placeholder="Name"
+                            defaultValue={data?.name}
+                            {...register("name")}
+                        />
+                        <Input
+                            type="text"
+                            className="mt-5"
+                            placeholder="Description"
+                            defaultValue={data?.description}
+                            {...register("description")}
+                        />
+                        <Input
+                            type="text"
+                            className="mt-5"
+                            placeholder="Image"
+                            defaultValue={data?.image}
+                            {...register("image")}
+                        />
+                        <Input
+                            type="text"
+                            className="mt-5"
+                            placeholder="Year"
+                            defaultValue={data?.year}
+                            {...register("year")}
+                        />
+                        <div className="mt-5">
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    className="mr-2"
+                                    defaultChecked={data?.isAvaiable}
+                                    {...register("isAvaiable")}
+                                />
+                                Is Available
+                            </label>
+                        </div>
+                        <Input
+                            type="text"
+                            className="mt-5"
+                            placeholder="Items in Stock"
+                            defaultValue={data?.itemsInStock}
+                            {...register("itemsInStock")}
+                        />
 
-						<Button
-							className="mt-6"
-							variant={"secondary"}
-							size={"lg"}
-						>
-							Upraviť knihu
-						</Button>
-					</form>
-				</div>
-			</SheetHelper>
-			<div className="ml-4">
-				<SheetHelper
-					variantProp="outline"
-					title="Zmazať knihu"
-					secondTitle="Chcete zmazať knihu"
-				>
-					<span className="mt-2 font-bold text-xl">
-						Chcte zmazať knihu: {name}
-					</span>
-					<div className="p-5 mt-5">
-						<form onSubmit={deleteBookFn}>
-							<Button variant={"destructive"} size={"lg"}>
-								Zmazať
-							</Button>
-						</form>
-					</div>
-				</SheetHelper>
-			</div>
-		</div>
-	);
+                        <Button
+                            className="mt-6"
+                            variant={"secondary"}
+                            size={"lg"}
+                            type="submit"
+                        >
+                            Update Book
+                        </Button>
+                    </form>
+                </div>
+            </SheetHelper>
+            <div className="ml-4">
+                <SheetHelper
+                    variantProp="outline"
+                    title="Delete Book"
+                    secondTitle="Are you sure you want to delete this book?"
+                >
+                    <span className="mt-2 font-bold text-xl">
+                        Are you sure you want to delete book: {name}
+                    </span>
+                    <div className="p-5 mt-5">
+                        <form onSubmit={deleteBookFn}>
+                            <Button variant={"destructive"} size={"lg"} type="submit">
+                                Delete
+                            </Button>
+                        </form>
+                    </div>
+                </SheetHelper>
+            </div>
+        </div>
+    );
 };
 
 export default BookSheets;
