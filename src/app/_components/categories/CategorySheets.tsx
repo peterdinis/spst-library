59d2984fe3efb0d92040d010f521d58c16fc8580
Peter Dinis <peterdinis0@pobox.sk>
2024-05-25 15:55:24 +0200

    feat: Start to update sheets WIP

--- src/app/_components/categories/CategorySheets.tsx
+++ src/app/_components/categories/CategorySheets.tsx
@@ -1,4 +1,4 @@
-import { FC, FormEvent } from "react";
+import { FC, FormEvent, useState } from "react";
 import { Button } from "~/components/ui/button";
 import SheetHelper from "../shared/SheetHelper";
 import { Category } from "@prisma/client";
@@ -6,91 +6,114 @@ import { Input } from "~/components/ui/input";
 import { api } from "~/trpc/react";
 import { useRouter } from "next/navigation";
 import { useToast } from "~/components/ui/use-toast";
-import { Form } from "~/components/ui/form";
 
 interface ICategorySheetsProps {
-	name: string;
-	data: Category;
+    name: string;
+    data: Category;
 }
 
 const CategorySheets: FC<ICategorySheetsProps> = ({
-	name,
-	data,
+    name,
+    data,
 }: ICategorySheetsProps) => {
-	const deleteCategoryMut = api.category.deleteCategory.useMutation();
-	const { toast } = useToast();
-	const router = useRouter();
+    const deleteCategoryMut = api.category.deleteCategory.useMutation();
+    const updateCategoryMut = api.category.updateCategory.useMutation();
+    const { toast } = useToast();
+    const router = useRouter();
 
-	const deleteCategoryFn = async (e: FormEvent) => {
-		e.preventDefault();
+    const [categoryName, setCategoryName] = useState(data?.name || "");
+    const [categoryDescription, setCategoryDescription] = useState(data?.description || "");
 
-		await deleteCategoryMut.mutateAsync({
-			id: data?.id,
-		});
+    const deleteCategoryFn = async (e: FormEvent) => {
+        e.preventDefault();
 
-		toast({
-			title: "Kategória bola úspešené zmazaná",
-			duration: 2000,
-			className: "bg-green-500",
-		});
-		router.push("/categories");
-	};
+        await deleteCategoryMut.mutateAsync({
+            id: data?.id,
+        });
 
-	return (
-		<div className="flex mt-5">
-			<SheetHelper
-				variantProp="default"
-				title={"Upraviť kategóriu"}
-				secondTitle={"Upraviť kategóriu"}
-			>
-				<span className="mt-2 font-bold text-xl">
-					Uprava kategórie: {name}
-				</span>
-				<div>
-					<form>
-						<Input
-							type="text"
-							className="mt-5"
-							placeholder="Meno"
-							value={data?.name}
-						/>
-						<Input
-							type="text"
-							className="mt-5"
-							placeholder="Popis"
-							value={data?.description}
-						/>
+        toast({
+            title: "Kategória bola úspešne zmazaná",
+            duration: 2000,
+            className: "bg-green-500",
+        });
+        router.push("/categories");
+    };
 
-						<Button
-							className="mt-6"
-							variant={"secondary"}
-							size={"lg"}
-						>
-							Upraviť kategóriu
-						</Button>
-					</form>
-				</div>
-			</SheetHelper>
-			<div className="ml-4">
-				<SheetHelper
-					variantProp="outline"
-					title="Zmazať kategóriu"
-					secondTitle="Chcete zmazať kategóriu"
-				>
-					<span className="mt-2 font-bold text-xl">
-						Chcte zmazať kategóriu: {name}
-					</span>
-					<div className="p-5 mt-5">
-						<form onSubmit={deleteCategoryFn}>
-							<Button variant={"destructive"} size={"lg"}>
-								Zmazať
-							</Button>
-						</form>
-					</div>
-				</SheetHelper>
-			</div>
-		</div>
-	);
+    const updateCategoryFn = async (e: FormEvent) => {
+        e.preventDefault();
+
+        await updateCategoryMut.mutateAsync({
+            id: data?.id,
+            name: categoryName,
+            description: categoryDescription,
+        });
+
+        toast({
+            title: "Kategória bola úspešne upravená",
+            duration: 2000,
+            className: "bg-green-500",
+        });
+        router.push("/categories");
+    };
+
+    return (
+        <div className="flex mt-5">
+            <SheetHelper
+                variantProp="default"
+                title={"Upraviť kategóriu"}
+                secondTitle={"Upraviť kategóriu"}
+            >
+                <span className="mt-2 font-bold text-xl">
+                    Úprava kategórie: {name}
+                </span>
+                <div>
+                    <form onSubmit={updateCategoryFn}>
+                        <Input
+                            type="text"
+                            className="mt-5"
+                            placeholder="Meno"
+                            value={categoryName}
+                            onChange={(e) => setCategoryName(e.target.value)}
+                        />
+                        <Input
+                            type="text"
+                            className="mt-5"
+                            placeholder="Popis"
+                            value={categoryDescription}
+                            onChange={(e) => setCategoryDescription(e.target.value)}
+                        />
+
+                        <Button
+                            className="mt-6"
+                            variant={"secondary"}
+                            size={"lg"}
+                            type="submit"
+                        >
+                            Upraviť kategóriu
+                        </Button>
+                    </form>
+                </div>
+            </SheetHelper>
+            <div className="ml-4">
+                <SheetHelper
+                    variantProp="outline"
+                    title="Zmazať kategóriu"
+                    secondTitle="Chcete zmazať kategóriu"
+                >
+                    <span className="mt-2 font-bold text-xl">
+                        Chcete zmazať kategóriu: {name}
+                    </span>
+                    <div className="p-5 mt-5">
+                        <form onSubmit={deleteCategoryFn}>
+                            <Button variant={"destructive"} size={"lg"}>
+                                Zmazať
+                            </Button>
+                        </form>
+                    </div>
+                </SheetHelper>
+            </div>
+        </div>
+    );
 };
 
 export default CategorySheets;
