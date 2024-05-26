c511386b0abf5c286b2a9f578e547c90fafcfe58
Peter Dinis <peterdinis0@pobox.sk>
2024-05-26 15:02:29 +0200

    feat: Dialog alert window to sheets

--- src/app/_components/shared/SheetHelper.tsx
+++ src/app/_components/shared/SheetHelper.tsx
@@ -1,52 +1,85 @@
 "use client";
 
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogCancel,
+  AlertDialogAction,
+} from "@radix-ui/react-alert-dialog";
 import { FC, ReactNode } from "react";
+import {
+  AlertDialogHeader,
+  AlertDialogFooter,
+} from "~/components/ui/alert-dialog";
 import { Button } from "~/components/ui/button";
 import {
-	Sheet,
-	SheetContent,
-	SheetHeader,
-	SheetTitle,
-	SheetDescription,
-	SheetTrigger,
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetDescription,
+  SheetTrigger,
 } from "~/components/ui/sheet";
 
 type BtnVariants =
-	| "default"
-	| "destructive"
-	| "outline"
-	| "secondary"
-	| "ghost"
-	| "link";
+  | "default"
+  | "destructive"
+  | "outline"
+  | "secondary"
+  | "ghost"
+  | "link";
 
 interface ISheetHelperProps {
-	children?: ReactNode;
-	title: string;
-	secondTitle: string;
-	variantProp: BtnVariants;
+  children?: ReactNode;
+  title: string;
+  secondTitle: string;
+  variantProp: BtnVariants;
 }
 
 const SheetHelper: FC<ISheetHelperProps> = ({
-	children,
-	title,
-	secondTitle,
-	variantProp,
+  children,
+  title,
+  secondTitle,
+  variantProp,
 }) => {
-	return (
-		<Sheet>
-			<SheetTrigger>
-				<Button variant={variantProp} size="lg">
-					{title}
-				</Button>
-			</SheetTrigger>
-			<SheetContent>
-				<SheetHeader>
-					<SheetTitle>{secondTitle}</SheetTitle>
-					<SheetDescription>{children}</SheetDescription>
-				</SheetHeader>
-			</SheetContent>
-		</Sheet>
-	);
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant={variantProp} size="lg">
+          {title}
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>{secondTitle}</SheetTitle>
+          <SheetDescription>{children}</SheetDescription>
+        </SheetHeader>
+
+        <AlertDialog>
+          <AlertDialogTrigger>Info pred upravovaním</AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>
+                Skontrolujte si či daná vec nemá referenciu niekde inde.
+              </AlertDialogTitle>
+              <AlertDialogDescription>
+                Ak mám vyvorenú kategóriu 'abc' a kategória 'abc' referencuje sa
+                v knihe 'Abc' nemôžeme zmazať kategóiu 'abc'. Najprv musíte v
+                knihe 'Abc' nahradiť kategóriu
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Zavrieť</AlertDialogCancel>
+              <AlertDialogAction>OK</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default SheetHelper;
