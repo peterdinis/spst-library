14faee9517400d6cd2782c40e03aea23c91d6721
Peter Dinis <peterdinis0@pobox.sk>
2024-06-03 12:44:21 +0200

    FOO

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,11 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-import { env } from "~/env";
-
 const createPrismaClient = () =>
 	new PrismaClient({
 		log:
-			env.NODE_ENV === "development"
+			process.env.NODE_ENV === "development"
 				? ["query", "error", "warn"]
 				: ["error"],
 	});
@@ -16,4 +14,4 @@ const globalForPrisma = globalThis as unknown as {
 
 export const db = globalForPrisma.prisma ?? createPrismaClient();
 
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
