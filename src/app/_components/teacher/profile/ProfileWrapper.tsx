cd6da39e05c25dedf45234aa18e403de51e70eda
Peter Dinis <peterdinis0@pobox.sk>
2024-06-18 21:25:38 +0200

    FOO

--- src/app/_components/teacher/profile/ProfileWrapper.tsx
+++ src/app/_components/teacher/profile/ProfileWrapper.tsx
@@ -1,17 +1,17 @@
-"use client";
+"use client"
 
-import Cookie from "js-cookie";
 import { useRouter } from "next/navigation";
 import type { FC } from "react";
 import type { ICookieAuthType } from "~/app/types/authTypes";
 import useTeacherCookie from "~/hooks/useTeacherCookie";
 import BorrowedBooks from "./BorrowedBooks";
 import Settings from "./Settings";
+import { getCookie } from "cookies-next";
 
 const ProfileWrapper: FC = () => {
 	const teacherCookie = useTeacherCookie();
 	const router = useRouter();
-	const teacherCheck = Cookie.get("isTeacherLogin");
+	const teacherCheck = getCookie("isTeacherLogin");
 
 	if (!teacherCheck) {
 		router.push("/not-allowed");
