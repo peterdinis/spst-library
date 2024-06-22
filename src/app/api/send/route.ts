3a5ab692820c4f48937b90a4acc294d8b6bda776
Peter Dinis <peterdinis0@pobox.sk>
2024-06-22 23:02:22 +0200

    FOO

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,15 +1,18 @@
 
+import { NextRequest } from 'next/server';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_AUTH_KEY);
 
-export async function POST() {
+export async function POST(req: NextRequest) {
+  const body = await req.json();
   try {
+    /* TODO: Dostať tu email a nahradiť from a dostať tu aj inú hodnotu pre to */
     const { data, error } = await resend.emails.send({
       from: 'onboarding@resend.dev',
       to: process.env.NEXT_PUBLIC_TESTING_EMAIL as unknown as string,
-      subject: "SKUSKA",
-      text: "SOSOSOSO"
+      subject: "Potvrdenie registrácie",
+      text: "Dobrý deň ..... Vaša registrácia do applikácie bola úspešná. Prajem pekný deň. Admin SPŠT Knižnica"
     });
 
     if (error) {
