3ab8fea43642bf56b533c01a2f7403f1d98207bc
Peter Dinis <peterdinis0@pobox.sk>
2024-06-23 10:48:57 +0200

    FOO

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,8 +1,6 @@
-"use client"
 
 import type { NextRequest } from "next/server";
 import { Resend } from "resend";
-import { useToast } from "~/components/ui/use-toast";
 import { getGreeting } from "~/lib/getHour";
 
 const resend = new Resend(process.env.RESEND_AUTH_KEY);
@@ -10,24 +8,18 @@ const resend = new Resend(process.env.RESEND_AUTH_KEY);
 const greeting = getGreeting();
 
 export async function POST(req: NextRequest) {
-	const {toast} = useToast();
 	const body = await req.json();
 
 	const email = body.email;
 	try {
 		const { data, error } = await resend.emails.send({
-			from: "onboarding@resend.dev",
+			from: "onboarding@resend.dev", // TODO: Zmeniť doménu neskôr
 			to: email,
 			subject: "Potvrdenie registrácie",
 			text: `${greeting} ${email}. Vaša registrácia do applikácie bola úspešná. S pozdravom Admin SPŠT Knižnica.`,
 		});
 
 		if (error) {
-			toast({
-				title: "Nastala chyba pri posielaní emailu",
-				duration: 2000,
-				className: "bg-red-500 text-white font-bold"
-			})
 			return Response.json({ error }, { status: 500 });
 		}
 		return Response.json(data);
