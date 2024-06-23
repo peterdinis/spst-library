a5dfd15b8e2300259ce8d3b5cf6bd1b29f3687fe
Peter Dinis <peterdinis0@pobox.sk>
2024-06-23 10:41:49 +0200

    feat: Updating route

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,5 +1,8 @@
+"use client"
+
 import type { NextRequest } from "next/server";
 import { Resend } from "resend";
+import { useToast } from "~/components/ui/use-toast";
 import { getGreeting } from "~/lib/getHour";
 
 const resend = new Resend(process.env.RESEND_AUTH_KEY);
@@ -7,11 +10,11 @@ const resend = new Resend(process.env.RESEND_AUTH_KEY);
 const greeting = getGreeting();
 
 export async function POST(req: NextRequest) {
+	const {toast} = useToast();
 	const body = await req.json();
 
 	const email = body.email;
 	try {
-		/* TODO: Dostať tu email a nahradiť from a dostať tu aj inú hodnotu pre to */
 		const { data, error } = await resend.emails.send({
 			from: "onboarding@resend.dev",
 			to: email,
@@ -20,10 +23,13 @@ export async function POST(req: NextRequest) {
 		});
 
 		if (error) {
-			console.log(error);
+			toast({
+				title: "Nastala chyba pri posielaní emailu",
+				duration: 2000,
+				className: "bg-red-500 text-white font-bold"
+			})
 			return Response.json({ error }, { status: 500 });
 		}
-		console.log(data);
 		return Response.json(data);
 	} catch (error) {
 		return Response.json({ error }, { status: 500 });
